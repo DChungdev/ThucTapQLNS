2bb219cbf5872c142085a081a900fc432c2abb22
Nguyen Duc Chung <nchung1020@gmail.com>
2024-08-08 21:09:47 +0700

    Push QL Employee's infor

---
+++ QuanLyNhanSu/QuanLyNhanSu/QuanLyNhanSu.WebAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuanLyNhanSu.Business.Interfaces;
+using QuanLyNhanSu.Data.Context;
+using QuanLyNhanSu.Models.Entities;
+
+namespace QuanLyNhanSu.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            var employees = await _employeeService.GetAllEmployeeAsync();
+            return Ok(employees);
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            var employee = await _employeeService.GetEmployeeByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateEmployee(Employee employee)
+        {
+            await _employeeService.AddEmployeeAsync(employee);
+            return CreatedAtAction(nameof(CreateEmployee), new { id = employee.AccountId }, employee);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> UpdateEmployee(int id, Employee employee)
+        {
+            if(id != employee.EmployeeId)
+            {
+                return BadRequest();
+            }
+            await _employeeService.UpdateEmployeeAsync(employee);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAppointment(int id)
+        {
+            await _employeeService.DeleteEmployeeAsync(id);
+            return NoContent();
+        }
+    }
+}
